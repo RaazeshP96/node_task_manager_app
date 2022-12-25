5ce88b8ef7ee152d3d56a43727b62d61d09b7c6e
raazeshp96 <raazeshp96@gmail.com>
2022-12-26 01:24:22 +0545

    fix:routes for task

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require("express");
-
 const app = express();
+const taskRouter = require("./routes/task");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world!");
-});
-
-// app.get("/api/v1/tasks", (req, res) => {});
+app.use("/api/v1/tasks", taskRouter);
 // app.post("/api/v1/tasks", (req, res) => {});
 // app.get("/api/v1/tasks/:id", (req, res) => {});
 // app.put("/api/v1/tasks/:id", (req, res) => {});
@@ -17,5 +13,3 @@ app.listen(
   port,
   console.log(`Server is listening on http://localhost:${port}`)
 );
-
-console.log("hello world!");
--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/v1/tasks", (req, res) => {});
+router.get("/", (req, res) => {
+  console.log("Task manager");
+  res.send("Task manager");
+});
 // router.post("/api/v1/tasks", (req, res) => {});
 // router.get("/api/v1/tasks/:id", (req, res) => {});
 // router.put("/api/v1/tasks/:id", (req, res) => {});
 // router.delete("/api/v1/tasks/:id", (req, res) => {});
+
+module.exports = router;

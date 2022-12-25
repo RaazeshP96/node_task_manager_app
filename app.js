dbee6feb1d79e74ea076cd25300e0a6bd35688fc
raazeshp96 <raazeshp96@gmail.com>
2022-12-26 01:16:10 +0545

    feat:general router setup

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello world!");
 });
 
+// app.get("/api/v1/tasks", (req, res) => {});
+// app.post("/api/v1/tasks", (req, res) => {});
+// app.get("/api/v1/tasks/:id", (req, res) => {});
+// app.put("/api/v1/tasks/:id", (req, res) => {});
+// app.delete("/api/v1/tasks/:id", (req, res) => {});
+
 const port = 8080;
 app.listen(
   port,
---
+++ routes/task.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/api/v1/tasks", (req, res) => {});
+// router.post("/api/v1/tasks", (req, res) => {});
+// router.get("/api/v1/tasks/:id", (req, res) => {});
+// router.put("/api/v1/tasks/:id", (req, res) => {});
+// router.delete("/api/v1/tasks/:id", (req, res) => {});

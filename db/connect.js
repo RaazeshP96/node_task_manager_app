63221f90109710d0d3a354440d51db9dca496f1e
raazeshp96 <raazeshp96@gmail.com>
2022-12-26 21:56:25 +0545

    feat:added mongoose

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,12 @@
+// getting-started.js
+const mongoose = require("mongoose");
+
 const connectionString = `mongodb+srv://raazeshp9:<password>@cluster0.0jyoc8c.mongodb.net/?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(connectionString);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
